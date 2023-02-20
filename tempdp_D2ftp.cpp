0422bd351678530dd40c8857eeead4297449b77d
g1thubhack3r <125267748+g1thubhack3r@users.noreply.github.com>
2023-02-20 20:37:10 +0800

    Create tempdp_D2ftp.cpp

---
+++ tempdp_D2ftp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+const int N = 100 + 5;
+bool dp[N];
+int w[N];
+int main()
+{
+	int n, W;
+	cin >> n;
+	for (int i = 1;i <= n;i++)
+	{
+		cin >> w[i];
+	}
+	cin >> W;
+	dp[0] = true;
+	for (int i = 1;i <= n;i++)
+	{
+		for (int j = 0;j <= n)
+	}
+}
