82e13e4069dc8ed2d1877d5eebf3d8f2b6583286
g1thubhack3r <125267748+g1thubhack3r@users.noreply.github.com>
2023-02-24 12:15:29 +0800

    Update tempdp_D2ftp.cpp

--- tempdp_D2ftp.cpp
+++ tempdp_D2ftp.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-const int N = 100 + 5;
+const int N_w = 100 + 5;
+const int N = 10000 + 5;
 bool dp[N];
-int w[N];
+int w[N_w];
 int main()
 {
 	int n, W;
@@ -15,6 +17,22 @@ int main()
 	dp[0] = true;
 	for (int i = 1;i <= n;i++)
 	{
-		for (int j = 0;j <= n)
+		for (int j = 0;j <= W;j++)
+		{
+			if (dp[j] && (j + w[i]) <= W)
+			{
+				dp[w[i] + j] = true;
+			}
+		}
 	}
+	int idx = 0;
+	for (int i = 0;i <= W;i++)
+	{
+		if (dp[i] && i >= idx)
+		{
+			idx = i;
+		}
+	}
+	cout << dp[idx] << endl;
+	return 0;
 }
